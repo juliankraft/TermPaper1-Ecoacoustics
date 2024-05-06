7a0cf247a9e811da3019d27217735cd70705b5ec
juliankraft <kraftjulian@gmail.com>
2024-05-06 09:30:44 +0200

    cleanup

--- Code/model_20.py
+++ Code/model_20.py
@@ -1,4 +1,5 @@
 import torch
+from lightning import LightningModule
 
 class ResBlock(torch.nn.Module):
     def __init__(self, in_channels: int, out_channels: int, kernel_size: int, n_max_pool: int, **kwargs):
@@ -22,9 +23,20 @@ class ResBlock(torch.nn.Module):
         return self.maxpool(out)
 
 
-class ResNet(torch.nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, n_max_pool: int, n_res_blocks: int, num_classes: int, **kwargs):
+class ResNet(LightningModule):
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            kernel_size: int,
+            n_max_pool: int,
+            n_res_blocks: int,
+            num_classes: int,
+            learning_rate: float = 0.001,
+            **kwargs):
         super().__init__()
+
+        self.learning_rate = learning_rate
 
         self.conv1 = torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding='same', **kwargs)
         self.batchnorm1 = torch.nn.BatchNorm2d(num_features=out_channels)
@@ -37,6 +49,8 @@ class ResNet(torch.nn.Module):
         self.convout = torch.nn.Conv2d(in_channels=out_channels, out_channels=num_classes, kernel_size=1, **kwargs)
 
         self.softmax = torch.nn.Softmax(dim=1)
+
+        self.cross_entropy_loss_fn = torch.nn.CrossEntropyLoss()
 
     def forward(self, x):
         # x: (N, C=1, H, W)
@@ -62,3 +76,42 @@ class ResNet(torch.nn.Module):
         out = self.softmax(out)
 
         return out
+
+    def accuracy(self, y_hat, y):
+        labels_hat = torch.argmax(y_hat, dim=1)
+        labels = torch.argmax(y, dim=1)
+        return torch.sum(labels == labels_hat).item() / (len(y) * 1.0)
+
+    def cross_entropy(self, y_hat, y):
+        return self.cross_entropy_loss_fn(y_hat, y.float())
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        # it is independent of forward
+        x, y = batch
+
+        y_hat = self(x)
+
+        loss = self.cross_entropy(y_hat, y)
+
+        # Logging to TensorBoard (if installed) by default
+        self.log_dict({'train_loss': loss, 'train_acc': self.accuracy(y_hat, y)})
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        # it is independent of forward
+        x, y = batch
+
+        y_hat = self(x)
+
+        loss = self.cross_entropy(y_hat, y)
+
+        # Logging to TensorBoard (if installed) by default
+        self.log_dict({'val_loss': loss, 'val_acc': self.accuracy(y_hat, y)})
+
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=self.learning_rate, weight_decay=0.0)
