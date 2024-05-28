03473620fc4918b1da35593c0158161e40975bd2
bask0 <29065166+bask0@users.noreply.github.com>
2024-05-28 22:16:48 +0200

    added experiment routine

--- run_experiment.py
+++ run_experiment.py
@@ -1,7 +1,49 @@
 import os
+from argparse import ArgumentParser
 
-
+PARAM_LIST = [
+    '--n_mels -1 --n_res_blocks 3 --learning_rate 0.001',
+    '--n_mels -1 --n_res_blocks 3 --learning_rate 0.01',
+    '--n_mels -1 --n_res_blocks 4 --learning_rate 0.001',
+    '--n_mels -1 --n_res_blocks 4 --learning_rate 0.01',
+    '--n_mels 64 --n_res_blocks 3 --learning_rate 0.001',
+    '--n_mels 64 --n_res_blocks 3 --learning_rate 0.01',
+    '--n_mels 64 --n_res_blocks 4 --learning_rate 0.001',
+    '--n_mels 64 --n_res_blocks 4 --learning_rate 0.01',
+]
 
 if __name__ == '__main__':
-    
 
+    parser = ArgumentParser()
+    parser.add_argument('--overwrite', action='store_true', help='Overwrite existing run')
+    parser.add_argument('--dryrun', action='store_true', help='Print commands without executing')
+    parser.add_argument('--dev', action='store_true', help='Quick dev run.')
+    parser.add_argument('--hp_ids', type=int, nargs='+', required=True, help='HPs to evaluate')
+
+    args = parser.parse_args()
+    hp_ids = args.hp_ids
+
+    num_hp_ids = len(PARAM_LIST)
+
+    commands = []
+    for hp_id in hp_ids:
+        if not (0 <= hp_id < num_hp_ids):
+            raise ValueError(
+                f'invalid `hp_id` detected: `hp_id={hp_id}` is out of range (0, {num_hp_ids - 1}).'
+            )
+
+        command = f'python code/training.py {PARAM_LIST[hp_id]}'
+
+        if args.overwrite:
+            command += ' --overwrite'
+
+        if args.dev:
+            command += ' --dev'
+
+        commands.append(command)
+
+    for command in commands:
+        if args.dryrun:
+            print(command)
+        else:
+            os.system(command)
